2b2bed01808b17103ac3f36f333d3bd7f2f18631
Bruno Santos <bside89@live.com>
2026-06-18 20:08:23 -0300

    feat: solve problem 001: two sum

---
+++ 001-two-sum/001.go
@@ -0,0 +1,14 @@
+// Link: https://leetcode.com/problems/two-sum/
+package main_001_two_sum
+
+func twoSum(nums []int, target int) []int {
+	numsMapped := make(map[int]int)
+	for i, num := range nums { // O(n)
+		numToSearch := target - num
+		if k, ok := numsMapped[numToSearch]; ok {
+			return []int{k, i}
+		}
+		numsMapped[num] = i
+	}
+	return nil
+}
