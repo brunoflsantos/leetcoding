1ab17086cb8dd5c1da6e73d8ab8e33a5785b151b
Bruno Santos <bside89@live.com>
2026-06-20 00:04:00 -0300

    feat: solve problem 704: binary search

---
+++ 704-binary-search/704.go
@@ -0,0 +1,22 @@
+// Link: https://leetcode.com/problems/binary-search/
+package binarysearch
+
+// Complexity: O(log n)
+func search(nums []int, target int) int {
+	return _search(nums, 0, len(nums)-1, target)
+}
+
+func _search(nums []int, i int, j int, target int) int {
+	if i > j { // Array size 1: element not found
+		return -1
+	}
+	midIndex := i + (j-i)/2
+	midElement := nums[midIndex]
+	if target == midElement {
+		return midIndex
+	} else if target > midElement {
+		return _search(nums, midIndex+1, j, target)
+	} else {
+		return _search(nums, i, midIndex-1, target)
+	}
+}
