69ab3d5e6c58f222fe6246d74e5a749fd771ddf9
Bruno Santos <bside89@live.com>
2026-06-20 04:46:56 -0300

    chore: optimize code 1732

--- 1732-find-the-highest-altitude/1732.go
+++ 1732-find-the-highest-altitude/1732.go
@@ -6,14 +6,10 @@ import "math"
 // Complexity: O(n)
 func largestAltitude(gain []int) int {
 	highest := 0
-	altitudes := make([]int, len(gain)+1)
-	altitudes[0] = 0
+	altitude := 0
 	for i := range gain {
-		j := i + 1
-		if j <= len(gain) {
-			altitudes[j] = altitudes[i] + gain[i]
-			highest = int(math.Max(float64(highest), float64(altitudes[j])))
-		}
+		altitude = altitude + gain[i]
+		highest = int(math.Max(float64(highest), float64(altitude)))
 	}
 	return highest
 }
